2851ebd9e9977be1a9e45ddba418979dddd475e4
jhovana <jomapi20@gmail.com>
2017-06-24 15:34:19 -0500

    My Django Girls app, first commit

---
+++ Taller/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jomap\desktop\taller django\djangogirls\taller\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
